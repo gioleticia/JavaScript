0f31425db7654ba613a45ae741e10634bf975b7d
gioleticia <giovana.raitz.mendes@escola.pr.gov.br>
2025-03-17 08:46:05 -0300

    Add files via upload

---
+++ coisa.js
@@ -0,0 +1,8 @@
+
+let nome = parseFloat(document.getElementById("nome").value)
+let idade= parseFloat(document.getElementById("idade").value)
+let rg =  parseFloat(document.getElementById("rg").value)
+let = endereco = parseFloat(document.getElementById("endereco").value)
+let profissao = parseFloat(document.getElementById("profissao").value)
+
+
